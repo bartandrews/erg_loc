fixed ener_stat_W_flow and hist disorder averaging

code/inst_ham.py
@@ -1,10 +1,10 @@
 # --- python imports
 import numpy as np
-import time
+from time import perf_counter
 import sys
 from joblib import delayed, Parallel
 # --- driven_systems imports
-from models.heisenberg import heisenberg
+import functions.func_ham as fh
 import functions.func_args as fa
 import functions.func_proc as fp
 
@@ -12,64 +12,82 @@ import functions.func_proc as fp
 def my_inst_ham(path_flag, threads, model, _leaf_args):
 
     path = "/data/baandr" if path_flag else ""  # specify the custom path
-    t0 = time.time()
+    t0 = perf_counter()  # start the timer
 
     leaf = fp.file_name_leaf("inst_ham", model, _leaf_args)
     sys.stdout = sys.stderr = fp.Logger("inst_ham", path, model, leaf)
 
-    tools = ["ener_spec", "ent_spec"]
+    tools = ["ener", "ener_spac", "ent"]
     data = fp.prepare_output_files(tools, path, model, leaf)
 
     ###################################################################################################################
 
     def realization(itr, _model, _leaf_args, entropy=False):
         print(f"Iteration {itr + 1} of {_leaf_args['dis']}")
-        if _model == "heisenberg":
-            H = heisenberg(_leaf_args['L'], _leaf_args['Nup'], _leaf_args['pauli'],
-                           _leaf_args['J'][0], _leaf_args['J'][1], _leaf_args['J'][2], _leaf_args['W'])
-        else:
-            raise ValueError("model not implemented in inst_ham")
+
+        H = fh.chosen_hamiltonian(_model, _leaf_args)
+
         if entropy:
             _E, psi = H.eigh()
+            _E_spac = []
+            for i in range(len(_E)-1):
+                _E_spac.append(_E[i+1]-_E[i])
+            _E_spac = np.asarray(_E_spac)
             _S = []
             for i in range(psi.shape[1]):
                 _S.append(H.basis.ent_entropy(psi[:, i], sub_sys_A=range(H.basis.L // 2))["Sent_A"])
-            return _E, _S
+            return _E, _E_spac, _S
         else:
             _E = H.eigvalsh()
-            return _E, None
+            _E_spac = []
+            for i in range(len(_E)-1):
+                _E_spac.append(_E[i+1]-_E[i])
+            _E_spac = np.asarray(_E_spac)
+            return _E, _E_spac, None
 
     ###################################################################################################################
 
-    ent_flag = True if "ent_spec" in tools else False
+    ent_flag = True if "ent" in tools else False
     array = np.asarray(Parallel(n_jobs=threads)(delayed(realization)(i, model, leaf_args, entropy=ent_flag)
                                                 for i in range(leaf_args['dis'])), dtype=object)
 
-    #############
-    # ener_spec #
-    #############
+    ########
+    # ener #
+    ########
 
-    if "ener_spec" in tools:
+    if "ener" in tools:
 
         E_array = array[:, 0]
         E = np.mean(E_array, axis=0)
 
         for i in E:
-            data['ener_spec'].write(f"{i}\n")
+            data['ener'].write(f"{i}\n")
 
-    ############
-    # ent_spec #
-    ############
+    #############
+    # ener_spac #
+    #############
 
-    if "ent_spec" in tools:
+    if "ener_spac" in tools:
 
-        S_array = array[:, 1]
+        E_spac_array = array[:, 1]
+        E_spac = np.concatenate(E_spac_array).ravel().tolist()
+
+        for i in E_spac:
+            data['ener_spac'].write(f"{i}\n")
+
+    #######
+    # ent #
+    #######
+
+    if "ent" in tools:
+
+        S_array = array[:, 2]
         S = np.mean(S_array, axis=0)
 
         for i in S:
-            data['ent_spec'].write(f"{i}\n")
+            data['ent'].write(f"{i}\n")
 
-    print("Total time taken (seconds) = ", time.time() - t0)
+    print(f"Total time taken (seconds) = {perf_counter()-t0:.1f}")
 
 
 if __name__ == "__main__":

code/standalone/XXZ/energy_spacing_statistics/XXZ_energy_spacing_statistics_plot.py
@@ -21,7 +21,7 @@ J_x_0 = 1
 J_z_0 = 1
 W_list = np.arange(0.5, 12.5, 0.5)
 # iteration parameters
-numb_itr = 10000  # 20000 for L=8,10 or 1000 for L=12,14
+numb_itr = 100  # 20000 for L=8,10 or 1000 for L=12,14
 numb_jobs = -1  # number of spawned processes used for parallelization
 
 
@@ -48,7 +48,6 @@ def realization(itr):
             delta_n = E[i]-E[i-1]
             delta_n_plus_1 = E[i+1]-E[i]
             r.append(min(delta_n, delta_n_plus_1)/max(delta_n, delta_n_plus_1))
-
         r_av.append(np.mean(r))
 
     return r_av
@@ -73,5 +72,5 @@ ax0.xaxis.set_major_formatter(FormatStrFormatter('$%g$'))
 ax0.set_ylabel("$[r^{(n)}_\\alpha]$")
 ax0.yaxis.set_major_formatter(FormatStrFormatter('$%g$'))
 
-plt.savefig(f"/home/bart/Documents/papers/MBF/XXZ/energy_spacing_statistics/XXZ_energy_spacing_statistics_plot_J_{J_z_0}.png", bbox_inches='tight', dpi=300)
+# plt.savefig(f"/home/bart/Documents/papers/MBF/XXZ/energy_spacing_statistics/XXZ_energy_spacing_statistics_plot_J_{J_z_0}.png", bbox_inches='tight', dpi=300)
 plt.show()

plot/ener_stat.py
@@ -14,15 +14,15 @@ def plot_ener_stat(_model, _file1, _file2=None, _save=False):
 
     proj_root = '/home/bart/PycharmProjects/erg_loc'
 
-    with open(os.path.join(proj_root, 'data/ener_spec', _model, _file1), 'r') as csvfile:
+    with open(os.path.join(proj_root, 'data/ener_spac', _model, _file1), 'r') as csvfile:
         plots = csv.reader(csvfile, delimiter='\t')
-        E_1 = []
+        E_spac_1 = []
         for i, row in enumerate(plots):
-            E_1.append(float(row[0]))
+            E_spac_1.append(float(row[0]))
 
     r_1 = []
-    for i in range(1, len(E_1) - 1):
-        r_1.append((E_1[i + 1] - E_1[i]) / (E_1[i] - E_1[i - 1]))
+    for i in range(1, len(E_spac_1)):
+        r_1.append(E_spac_1[i]/E_spac_1[i-1])
 
     if _file2 is None:
 
@@ -39,26 +39,26 @@ def plot_ener_stat(_model, _file1, _file2=None, _save=False):
         ax0.set_xlim([0, 5])
         ax0.set_ylabel("$P(r)$")
         ax0.yaxis.set_major_formatter(FormatStrFormatter('$%g$'))
-        ax0.set_title(_file1.replace('ener_spec_', '').replace('_', '\_').replace('.dat', ''))
+        ax0.set_title(_file1.replace('ener_spac_', '').replace('_', '\_').replace('.dat', ''))
 
         if _save:
             os.makedirs(os.path.join(proj_root, 'figures/ener_stat', _model), exist_ok=True)
             plt.savefig(os.path.join(proj_root, 'figures/ener_stat', _model,
-                                     _file1.replace('spec', 'stat').replace(".dat", ".png")),
+                                     _file1.replace('spac', 'stat').replace(".dat", ".png")),
                         bbox_inches='tight', dpi=300)
         plt.show()
 
     else:
 
-        with open(os.path.join(proj_root, 'data/ener_spec', _model, _file2), 'r') as csvfile:
+        with open(os.path.join(proj_root, 'data/ener_spac', _model, _file2), 'r') as csvfile:
             plots = csv.reader(csvfile, delimiter='\t')
-            E_2 = []
+            E_spac_2 = []
             for i, row in enumerate(plots):
-                E_2.append(float(row[0]))
+                E_spac_2.append(float(row[0]))
 
         r_2 = []
-        for i in range(1, len(E_2) - 1):
-            r_2.append((E_2[i+1]-E_2[i])/(E_2[i]-E_2[i-1]))
+        for i in range(1, len(E_spac_2)):
+            r_2.append(E_spac_2[i]/E_spac_2[i-1])
 
         plt.figure(figsize=(10, 5))
         gs = gridspec.GridSpec(1, 2, hspace=0, wspace=0.1)
@@ -76,7 +76,7 @@ def plot_ener_stat(_model, _file1, _file2=None, _save=False):
         ax0.set_xlim([0, 5])
         ax0.set_ylabel("$P(r)$")
         ax0.yaxis.set_major_formatter(FormatStrFormatter('$%g$'))
-        ax0.set_title(_file1.replace('ener_spec_', '').replace('_', '\_').replace('.dat', ''))
+        ax0.set_title(_file1.replace('ener_spac_', '').replace('_', '\_').replace('.dat', ''))
 
         ax1.yaxis.set_visible(False)
         ax1.hist(r_2, bins=np.arange(0, 5.1, 0.1), density=True)
@@ -85,12 +85,12 @@ def plot_ener_stat(_model, _file1, _file2=None, _save=False):
         ax1.legend(loc='upper right')
         ax1.set_xlabel("$r$")
         ax1.set_xlim([0, 5])
-        ax1.set_title(_file2.replace('ener_spec_', '').replace('_', '\_').replace('.dat', ''))
+        ax1.set_title(_file2.replace('ener_spac_', '').replace('_', '\_').replace('.dat', ''))
 
         if _save:
             os.makedirs(os.path.join(proj_root, 'figures/ener_stat', _model), exist_ok=True)
             plt.savefig(os.path.join(proj_root, 'figures/ener_stat', _model,
-                                     _file1.replace('spec', 'stat').replace(".dat", "_comparison.png")),
+                                     _file1.replace('spac', 'stat').replace(".dat", "_comparison.png")),
                         bbox_inches='tight', dpi=300)
         plt.show()
 
@@ -98,7 +98,7 @@ def plot_ener_stat(_model, _file1, _file2=None, _save=False):
 if __name__ == "__main__":
 
     model = 'heisenberg'
-    file1 = 'ener_spec_heisenberg_L_14_Nup_7_pauli_0_obc_J_1_1_1_W_0.5.dat'
-    file2 = 'ener_spec_heisenberg_L_14_Nup_7_pauli_0_obc_J_1_1_1_W_8.dat'
+    file1 = 'ener_spac_heisenberg_L_8_Nup_4_pauli_0_obc_dis_10000_J_1_1_1_W_0.5.dat'
+    file2 = 'ener_spac_heisenberg_L_8_Nup_4_pauli_0_obc_dis_10000_J_1_1_1_W_8.dat'
 
     plot_ener_stat(model, file1, file2, _save=True)